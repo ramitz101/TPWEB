594779b2745973608ec84fb285845b8f5a0d0f42
samuelbelisle <samuel_belisle@hotmail.com>
2017-10-31 12:29:16 -0400

    CategorieListFragment.kt

--- app/src/main/java/ca/qc/android/cstj/bibliothequemobile/helpers/services.kt
+++ app/src/main/java/ca/qc/android/cstj/bibliothequemobile/helpers/services.kt
@@ -2,4 +2,6 @@ package ca.qc.android.cstj.bibliothequemobile.helpers
 
 // ICI , il y aura tous les lien pour catégories, succursale,ect
 
+var CATEGORIE_URL = "https://tp1-samuelbelisle.c9users.io/categories"
+
 var LIVRE_URL =  "https://tp1-samuelbelisle.c9users.io/livres"
--- app/src/main/java/ca/qc/android/cstj/bibliothequemobile/models/Categorie.kt
+++ app/src/main/java/ca/qc/android/cstj/bibliothequemobile/models/Categorie.kt
@@ -6,5 +6,7 @@ import com.github.kittinunf.fuel.android.core.Json
  * Created by Administrateur on 2017-10-31.
  */
 class Categorie(jsonObject: Json) {
-
+    var categorieId : String = jsonObject.obj().getString("idCategorie")
+    var categorieUUID : String = jsonObject.obj().getString("uuid")
+    var categorie : String = jsonObject.obj().getString("categorie")
 }
