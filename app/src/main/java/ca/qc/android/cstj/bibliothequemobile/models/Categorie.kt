c59c15dbdbf7cbec6e2d8b5444f9da0414ecd10d
ramitz <ramitz101@gmail.com>
2017-10-31 12:32:54 -0400

    cc

--- app/src/main/java/ca/qc/android/cstj/bibliothequemobile/models/Categorie.kt
+++ app/src/main/java/ca/qc/android/cstj/bibliothequemobile/models/Categorie.kt
@@ -1,10 +1,14 @@
 package ca.qc.android.cstj.bibliothequemobile.models
 
 import com.github.kittinunf.fuel.android.core.Json
+import ca.qc.android.cstj.bibliothequemobile.models.Item
 
 /**
  * Created by Administrateur on 2017-10-31.
  */
-class Categorie(jsonObject: Json) {
+class Categorie(jsonObject: Json): Item() {
+    override fun getAffichage():String {
+
+    }
 
 }
